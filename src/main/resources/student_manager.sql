97d69d010ee83215ac054a5ac4956775a92e5596
iean-jpg <59051925+iean-jpg@users.noreply.github.com>
2021-05-24 15:20:09 +0800

    Add files via upload
    最终提交

--- src/main/resources/student_manager.sql
+++ src/main/resources/student_manager.sql
@@ -3,19 +3,36 @@
 
  Source Server         : localhost_3306
  Source Server Type    : MySQL
- Source Server Version : 50733
+ Source Server Version : 50734
  Source Host           : localhost:3306
  Source Schema         : student_manager
 
  Target Server Type    : MySQL
- Target Server Version : 50733
+ Target Server Version : 50734
  File Encoding         : 65001
 
- Date: 18/05/2021 23:13:08
+ Date: 19/05/2021 09:00:25
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for role
+-- ----------------------------
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role`  (
+  `uid` int(11) NOT NULL,
+  `utype` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`uid`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of role
+-- ----------------------------
+INSERT INTO `role` VALUES (1, 'admin');
+INSERT INTO `role` VALUES (2, 'teacher');
+INSERT INTO `role` VALUES (3, 'student');
 
 -- ----------------------------
 -- Table structure for score
@@ -27,7 +44,7 @@ CREATE TABLE `score`  (
   `android` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '',
   `jsp` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of score
@@ -59,7 +76,7 @@ CREATE TABLE `student`  (
   `major` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `email` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of student
@@ -67,16 +84,6 @@ CREATE TABLE `student`  (
 INSERT INTO `student` VALUES ('20192430634', '123', '老八', '男', '2019-9', '计算机科学与技术', 'zzh2910@qq.com');
 INSERT INTO `student` VALUES ('20192430635', '0', '厨子', '男', '2019-9', '计算机科学与技术', 'zzh2910@qq.com');
 INSERT INTO `student` VALUES ('20192430636', '0', '傻逼', '男', '2019-9', '软件工程', 'zzh2910@qq.com');
-INSERT INTO `student` VALUES ('20192430637', '0', '三松', '男', '2019-9', '计算机科学与技术', 'zzh2910@qq.com');
-INSERT INTO `student` VALUES ('20192430638', '0', '王贺龙', '男', '2019-9', '软件工程', 'zzh2910@qq.com');
-INSERT INTO `student` VALUES ('20192430639', '0', '张勃', '男', '2019-9', '计算机科学与技术', 'zzh2910@qq.com');
-INSERT INTO `student` VALUES ('20192430640', '0', '张红展', '男', '2019-9', '软件工程', 'zzh2910@qq.com');
-INSERT INTO `student` VALUES ('20192430641', '0', '赵旺奇', '女', '2019-9', '软件工程', 'zzh2910@qq.com');
-INSERT INTO `student` VALUES ('20192430642', '0', '王配', '男', '2019-9', '计算机科学与技术', 'zzh2910@qq.com');
-INSERT INTO `student` VALUES ('20192430643', '0', '大伟', '男', '2019-9', '软件工程', 'zzh2910@qq.com');
-INSERT INTO `student` VALUES ('20192430644', '0', '谢强', '男', '2019-09', '软件工程', NULL);
-INSERT INTO `student` VALUES ('20192430645', '0', '韦清兵', '男', '2019-09', '软件培训中心', NULL);
-INSERT INTO `student` VALUES ('20192430646', '0', '崔仓豪', '男', '2019-12', '计算机科学与技术', NULL);
 
 -- ----------------------------
 -- Table structure for teacher
@@ -89,7 +96,7 @@ CREATE TABLE `teacher`  (
   `sex` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '',
   `email` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of teacher
